abd10c3c08eaf9840436d93a468062114d1de098
chaitanya.tank <chaitanya.p@agileinfoways.com>
2022-02-03 12:26:18 +0530

    starting nodePort service

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 
@@ -8,23 +9,47 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  return res.send(posts);
-});
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
+const handleEvents = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
   }
   if (type === "CommentCreated") {
-    const { id, postId, content } = data;
-    posts[postId].comments.push({ id, content });
+    const { id, postId, content, status } = data;
+    posts[postId].comments.push({ id, content, status });
   }
-  console.table(posts);
+
+  if (type === "CommentUpdated") {
+    const { id, postId, status, content } = data;
+    const post = posts[postId];
+    const comment = post.comments.find((c) => c.id === id);
+
+    // we are already mutating the original object (pass by reference)
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get("/posts", (req, res) => {
+  return res.send(posts);
+});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  handleEvents(type, data);
+  console.dir(posts, { depth: null });
   res.send({});
 });
 
-app.listen("4002", () => {
+app.listen("4002", async () => {
   console.log("Listening on 4002");
+
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+    for (let event of res.data) {
+      console.log("Processing Event", event.type);
+      handleEvents(event.type, event.data);
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
 });
